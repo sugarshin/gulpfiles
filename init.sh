a8acac6b81165eb15cd59fc57ed2118ec9068407
sugarshin <shinsugar@gmail.com>
2015-06-16 14:15:30 +0900

    edit init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 npm i -D babel babelify browser-sync browserify coffeeify del gulp gulp-imagemin gulp-jade gulp-load-plugins gulp-minify-css gulp-notify gulp-plumber gulp-rename gulp-replace gulp-stylus gulp-uglify imagemin-pngquant nib require-dir run-sequence vinyl-source-stream watchify
 mkdir -p build dest src/html src/css src/js
-touch src/html/index.html src/css/index.css src/js/main.js
+touch src/html/index.jade src/css/index.styl src/js/main.js
 rm -rf .git init.sh
